8aa0aaf8fb39275b850637a64070e76f74b4ada2
Paul Moore <pdmoore@gmail.com>
2019-05-27 13:12:42 -0700

    starting point, tests run, no CI hooks

---
+++ src/main/java/dev/emergent/ISBNFinder.java
@@ -0,0 +1,36 @@
+package dev.emergent;
+
+public class ISBNFinder {
+
+    private BookInfoProvider isbnService = null;
+
+    public ISBNFinder() {
+        this(ISBNService.getInstance());
+    }
+
+    public ISBNFinder(BookInfoProvider bookInfoProvider) {
+        isbnService = bookInfoProvider;
+    }
+
+    public BookInfo lookup(String ISBN) {
+
+        if (ISBN.length() == 13) {
+            return isbnService.retrieve(ISBN);
+        }
+
+        if (ISBN.length() < 10) {
+            return new BookInfo("ISBN must be 10 characters in length");
+        } else if (ISBN.length() > 10) {
+            return new BookInfo("ISBN must be 10 characters in length");
+        } else {
+
+            BookInfo bookInfo = isbnService.retrieve(ISBN);
+
+            if (null == bookInfo) {
+                return new BookInfo("Title not found");
+            }
+
+            return bookInfo;
+      }
+    }
+}
---
+++ src/test/java/dev/emergent/ISBN10Tests.java
@@ -0,0 +1,59 @@
+package dev.emergent;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ISBN10Tests {
+
+    @Test
+    public void ISBN_shorterThan10Characters_ReturnsInvalidBookInfo() {
+
+        // Arrange
+        String shortISBN = "12345";
+
+        // Act
+        ISBNFinder sut = new ISBNFinder();
+        BookInfo actual = sut.lookup(shortISBN);
+
+        // Assert
+        assertEquals("ISBN must be 10 characters in length", actual.title);
+    }
+
+    @Test
+    public void ISBN_longerThan10Characters_ReturnsInvalidBookInfo() {
+
+        // Arrange
+        String longISBN = "1234567890BCDEF";
+
+        // Act
+        ISBNFinder sut = new ISBNFinder();
+        BookInfo actual = sut.lookup(longISBN);
+
+        // Assert
+        assertEquals("ISBN must be 10 characters in length", actual.title);
+    }
+
+    @Test
+    public void ISBN_BookNotFound() {
+
+        String unknownISBN = "0553562614";
+
+        ISBNFinder sut = new ISBNFinder();
+        BookInfo actual = sut.lookup(unknownISBN);
+
+        assertEquals("Title not found", actual.title);
+    }
+
+    @Test
+    public void ISBN_BookFound() {
+        String ISBN = "0321146530";
+
+        ISBNFinder sut = new ISBNFinder();
+        BookInfo actual = sut.lookup(ISBN);
+
+        BookInfo expected = new BookInfo("Test Driven Development by Example", "Kent Beck", "0321146530", "9780321146533");
+
+        assertEquals(expected.toString(), actual.toString());
+    }
+}
