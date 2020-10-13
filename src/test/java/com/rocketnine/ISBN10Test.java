61c3493adcd9b455cfcc738b66e19156ea8b9ac8
Paul Moore <pdmoore@gmail.com>
2020-10-13 12:58:47 -0700

    Bump JUnit to 5.6.2

--- src/test/java/com/rocketnine/ISBN10Test.java
+++ src/test/java/com/rocketnine/ISBN10Test.java
@@ -1,9 +1,9 @@
 package com.rocketnine;
 
 import dev.emergent.BookInfo;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ISBN10Test {
 
